ad149d8bec63e5fa5c2653f8d75287c2484a315f
BlaneRhodes <bcr76@txstate.edu>
2021-04-14 19:05:24 -0500

    System for placing structures created

--- Sandbox/src/GameLayer.h
+++ Sandbox/src/GameLayer.h
@@ -5,6 +5,10 @@
 #include "Player.h"
 #include "CrossPoint.h"
 
+enum class ActiveFrame
+{
+	NONE = -1, MAP = 0, PLAYER1_CARDS = 1, PLAYER2_CARDS = 2
+};
 
 class GameLayer : public Crumble::Layer 
 {
@@ -22,11 +26,27 @@ public:
 	bool OnWindowResize(Crumble::WindowResizeEvent& e);
 private:
 	void CreateCamera(uint32_t width, uint32_t height);
+	ActiveFrame GetActiveFrame(float x, float y);
+	void HandleMapEvents(float normX, float normY);
 private:
 	Crumble::Scope<Crumble::OrthographicCamera> m_Camera;
 	TileMap m_TileMap;
-	std::vector<std::vector<CrossPoint>> m_NodeMap;
+	std::vector<CrossPoint> m_NodeMap;
 	Crumble::Ref<Player> m_Player1;
 	Crumble::Ref<Player> m_Player2;
+	int m_CurrentPlayer = 0;
+	StructureType m_Selection = StructureType::ROAD;
+	bool m_MouseState = true;
+
+	float m_AspectRatio;
+	float m_CamWidth = 8.0f;
+	float m_Bottom = -m_CamWidth;
+	float m_Top = m_CamWidth;
+	float m_Left;
+	float m_Right;
+
+	glm::vec4 m_MapFrame = glm::vec4({-6.0f, 6.0f, -6.5f, 6.5f});
+	glm::vec4 m_Player1CardsFrame = glm::vec4({ -14.0f, -5.5f, -7.9f, -6.4f });
+	glm::vec4 m_Player2CardsFrame = glm::vec4({ 5.5f, 14.0f, -7.9f, -6.4f });
 	//ImFont* m_Font;
 };
--- Sandbox/src/Player.cpp
+++ Sandbox/src/Player.cpp
@@ -21,6 +21,29 @@ void Player::RemoveResource(Resource resource, uint32_t amount)
 	m_Resources.at(static_cast<int>(resource)) -= amount;
 }
 
+void Player::AddStructure(Structure structure)
+{
+	m_Structures.push_back(structure);
+}
+
+void Player::RemoveStructure(StructureType type, glm::vec2 position)
+{
+	
+	for (auto it = m_Structures.begin(); it != m_Structures.end(); it++)
+	{
+		Structure currStruct = *it;
+		float xPos = currStruct.position.x;
+		float yPos = currStruct.position.y;
+		bool matchedX = (std::abs(xPos) - position.x < 0.0001 && std::abs(xPos) - position.x > -0.0001);
+		bool matchedY = (std::abs(yPos) - position.y < 0.0001 && std::abs(yPos) - position.y > -0.0001);
+
+		if (currStruct.buildType == type && xPos && yPos)
+		{
+			m_Structures.erase(it);
+		}
+	}
+}
+
 void Player::DecrementScore(uint32_t value)
 {
 	m_Score = std::max((unsigned int)0, m_Score - value);
